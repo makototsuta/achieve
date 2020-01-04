fd423c654ee00458bbe2c61f48bcf4ffc46cdd28
makoto tsuta <makoto1850@gmail.com>
2020-01-04 21:26:15 +0900

    Strong Parameterを実装

--- RailsClone/app/controllers/clones_controller.rb
+++ RailsClone/app/controllers/clones_controller.rb
@@ -8,6 +8,14 @@ class ClonesController < ApplicationController
   end
 
   def create
+    Clone.create(clone_params)
+    redirect_to new_clone_path
+  end
+
+  private
+
+  def clone_params
+    params.require(:clone).permit(:content)
   end
 
 end
